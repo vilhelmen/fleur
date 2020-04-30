1994600f27ffcbfebda133a46efbf6b6efec1acd
Will Starms <vilhelmen@gmail.com>
2020-04-30 00:59:25 -0500

    well none of this supports color sanely because CURSES

--- fleur/__main__.py
+++ fleur/__main__.py
@@ -29,20 +29,20 @@ class myEmployeeForm(npyscreen.Form):
         self.myDate = self.add(npyscreen.TitleDateCombo, name='Date Employed')
 
 
-
 class GridObject:
-    _template = symbols.flower_stages
-
     show_bold = False  # set by grid because... editing cell???
     highlight = False  # set by grid on display to show highlight status
     grid_current_value_index = (0, 0)  # set by grid to indicate grid position(??) (row, col)
 
-    def __str__(self):
-        return self._template[2]
-
-
-
-
+    # TODO: only render when updating, serve a cached file. this will get asked to render A LOT by the library
+    def render(self):
+        if self.show_bold:
+            BORDER = symbols.yellow
+        elif self.highlight:
+            BORDER = symbols.cyan
+        else:
+            BORDER = symbols.brown
+        return [symbols.framed_flowers[self.grid_current_value_index[0] % 4].format(BORDER=BORDER, FLOWER=symbols.rose)]
 
 
 class FlowerGrid(npyscreen.SimpleGrid):
@@ -53,9 +53,12 @@ class FlowerGrid(npyscreen.SimpleGrid):
             for y in x:
                 y.autowrap = True
 
+    def display_value(self, vl):
+        return vl.render()
+
     def custom_print_cell(self, actual_cell, cell_display_value):
-        logger.debug('Displaying cell')
-        actual_cell.values = [str(cell_display_value)]
+        logger.debug('Displaying cell %s %s', type(cell_display_value), cell_display_value)
+        actual_cell.values = cell_display_value
 
 
 class PlayArea(npyscreen.Form):
@@ -64,7 +67,7 @@ class PlayArea(npyscreen.Form):
 
     def create(self):
         self.grid = self.add(FlowerGrid, columns=4, column_width=10, row_height=5, always_show_cursor=True)
-        self.grid.set_grid_values_from_flat_list([GridObject()]*20, max_cols=4)
+        self.grid.values = [[GridObject() for _ in range(5)] for _ in range(4)]
         #self.myDepartment = self.add(npyscreen.TitleSelectOne, scroll_exit=True, max_height=3, name='Department',
         #                             values=['Department 1', 'Department 2', 'Department 3'])
         #self.myDate = self.add(npyscreen.TitleDateCombo, name='Date Employed')
